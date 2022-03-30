1a858a46b59c975516656f0340a0999d92a155ad
Adam Davidson <agdavidson91@gmail.com>
2022-03-29 19:24:27 -0500

    added all the components and css and background image and added props to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import CreatePost from "./Components/CreatePost";
+import DisplayPosts from "./Components/DisplayPosts";
+import NavBar from "./Components/NavBar";
+import Post from "./Components/Post";
+
 function App() {
   return (
     <div>
       <h1>Social Feed</h1>
+      <CreatePost />
+      <DisplayPosts />
+      <NavBar />
+      <Post />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "./style.css";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
